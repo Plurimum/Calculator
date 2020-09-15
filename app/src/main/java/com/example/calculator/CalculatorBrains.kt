78e502e0179c0bd40bc49ba5f0b36d328e698076
Plurimum <maxlih272@gmail.com>
2020-09-15 22:34:59 +0300

    Added all files

---
+++ app/src/main/java/com/example/calculator/CalculatorBrains.kt
@@ -0,0 +1,202 @@
+package com.example.calculator
+
+import android.util.Log
+import java.lang.Math.abs
+
+open class CalculatorBrains {
+
+    private companion object {
+        const val DOTLENGTH: Int = 1
+        const val DIGITLENGTH: Int = 1
+        const val OPERATIONLENGTH: Int = 3
+    }
+
+    var firstArgument: Double = 0.0
+    var firstArgStr: String = ""
+    var firstArgHasDot: Boolean = false
+
+    var secondArgument: Double = 0.0
+    var secondArgStr: String = ""
+    var secondArgHasDot: Boolean = false
+
+    var operation: String = ""
+
+    var outputString: String = ""
+
+    fun concatDigit(number: String, buttonId: Int): String {
+        var tmpNumber = number
+        tmpNumber += when (buttonId) {
+            R.id.zero -> {
+                if (tmpNumber == "") {
+                    "0"
+                } else {
+                    if (tmpNumber != "0") {
+                        "0"
+                    } else {
+                        ""
+                    }
+                }
+            }
+            R.id.one -> "1"
+            R.id.two -> "2"
+            R.id.three -> "3"
+            R.id.four -> "4"
+            R.id.five -> "5"
+            R.id.six -> "6"
+            R.id.seven -> "7"
+            R.id.eight -> "8"
+            R.id.nine -> "9"
+            else -> ""
+        }
+        return tmpNumber
+    }
+
+    fun digitPressed(buttonId: Int) {
+        var isVeryLong = false
+        if (operation == "") {
+            if (firstArgStr != "" && firstArgStr.last() != '.') {
+                if (abs(firstArgStr.toDouble().toLong()) < 100_000_000) {
+                    firstArgStr = concatDigit(firstArgStr, buttonId)
+                } else {
+                    isVeryLong = true
+                }
+            } else {
+                firstArgStr = concatDigit(firstArgStr, buttonId)
+            }
+        } else {
+            if (secondArgStr != "" && secondArgStr.last() != '.') {
+                if (abs(secondArgStr.toDouble().toLong()) < 100_000_000) {
+                    secondArgStr = concatDigit(secondArgStr, buttonId)
+                } else {
+                    isVeryLong = true
+                }
+            } else {
+                secondArgStr = concatDigit(secondArgStr, buttonId)
+            }
+        }
+        if (!isVeryLong) {
+            outputString = concatDigit(outputString, buttonId)
+        }
+        Log.i("digit", "successfully pressed!")
+    }
+
+    fun compute() {
+        if (firstArgStr != "" && secondArgStr != "" && operation != "") {
+            Log.i("Compute", "started!")
+            firstArgument = firstArgStr.toDouble()
+            secondArgument = secondArgStr.toDouble()
+            var tmpResult: Double
+            tmpResult = when (operation) {
+                "+" -> firstArgument + secondArgument
+                "-" -> firstArgument - secondArgument
+                "*" -> firstArgument * secondArgument
+                "/" -> firstArgument / secondArgument
+                else -> 0.0
+            }
+            operation = ""
+            firstArgument = 0.0
+            secondArgument = 0.0
+            firstArgHasDot = tmpResult - tmpResult.toLong() != 0.0
+            secondArgHasDot = false
+            var tmpResultStr = removeLastZeroes(tmpResult)
+            firstArgStr = tmpResultStr
+            secondArgStr = ""
+            outputString = tmpResultStr
+            Log.i("Compute", "successfully!")
+        }
+    }
+
+    fun removeLastZeroes(number: Double): String {
+        var tmpNumberStr = number.toString()
+        while (tmpNumberStr.last() == '0') {
+            tmpNumberStr = tmpNumberStr.dropLast(DIGITLENGTH)
+        }
+        if (tmpNumberStr.last() == '.') {
+            tmpNumberStr = tmpNumberStr.dropLast(DOTLENGTH)
+        }
+        return tmpNumberStr
+    }
+
+    fun actionPressed(buttonId: Int) {
+        when (buttonId) {
+            R.id.divide -> {
+                if (operation == "") {
+                    operation = "/"
+                    outputString += " / "
+                }
+            }
+            R.id.multiply -> {
+                if (operation == "") {
+                    operation = "*"
+                    outputString += " * "
+                }
+            }
+            R.id.add -> {
+                if (operation == "") {
+                    operation = "+"
+                    outputString += " + "
+                }
+            }
+            R.id.subtract -> {
+                if (operation == "") {
+                    operation = "-"
+                    outputString += " - "
+                }
+            }
+            R.id.clear -> {
+                firstArgument = 0.0
+                secondArgument = 0.0
+                firstArgHasDot = false
+                secondArgHasDot = false
+                operation = ""
+                firstArgStr = ""
+                secondArgStr = ""
+                outputString = ""
+            }
+            R.id.delete -> {
+                if (outputString != "") {
+                    if (outputString.last().isDigit()) {
+                        if (operation == "") {
+                            firstArgStr = firstArgStr.dropLast(DIGITLENGTH)
+                            outputString = outputString.dropLast(DIGITLENGTH)
+                        } else {
+                            secondArgStr = secondArgStr.dropLast(DIGITLENGTH)
+                            outputString = outputString.dropLast(DIGITLENGTH)
+                        }
+                    } else {
+                        if (outputString.last() == ',') {
+                            if (operation == "") {
+                                firstArgHasDot = false
+                                firstArgStr = firstArgStr.dropLast(DOTLENGTH)
+                                outputString = outputString.dropLast(DOTLENGTH)
+                            } else {
+                                secondArgHasDot = false
+                                secondArgStr = secondArgStr.dropLast(DOTLENGTH)
+                                outputString = outputString.dropLast(DOTLENGTH)
+                            }
+                        } else {
+                            operation = ""
+                            outputString = outputString.dropLast(OPERATIONLENGTH)
+                        }
+                    }
+                }
+            }
+            R.id.dot -> {
+                if (firstArgStr != "" && !firstArgHasDot && operation == "") {
+                    firstArgHasDot = true
+                    firstArgStr += '.'
+                    outputString += '.'
+                }
+                if (secondArgStr != "" && !secondArgHasDot && operation != "") {
+                    secondArgHasDot = true
+                    secondArgStr += '.'
+                    outputString += '.'
+                }
+            }
+            R.id.equal -> {
+                Log.i("Equal", "pressed!")
+                compute()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var result: TextView
+
+    companion object {
+        const val FIRST_ARG_STR_KEY = "KEY of string performance of first arg"
+        const val FIRST_ARG_KEY = "KEY of first arg"
+        const val FIRST_ARG_HAS_DOT_KEY = "KEY of hasDot"
+
+        const val SECOND_ARG_STR_KEY = "bla bla"
+        const val SECOND_ARG_KEY = "keeeeeyyyyyy"
+        const val SECOND_ARG_HAS_DOT_KEY = "dootttttt"
+
+        const val OPERATION_KEY = "operation"
+
+        const val OUTPUT_STRING_KEY = "screen"
+    }
+
+    object calculator: CalculatorBrains()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        result = findViewById(R.id.result)
+
+        val digitButtons = arrayOf(
+            findViewById<Button>(R.id.zero),
+            findViewById(R.id.one),
+            findViewById(R.id.two),
+            findViewById(R.id.three),
+            findViewById(R.id.four),
+            findViewById(R.id.five),
+            findViewById(R.id.six),
+            findViewById(R.id.seven),
+            findViewById(R.id.eight),
+            findViewById(R.id.nine)
+        )
+
+        val digitIds = arrayOf(
+            R.id.zero,
+            R.id.one,
+            R.id.two,
+            R.id.three,
+            R.id.four,
+            R.id.five,
+            R.id.six,
+            R.id.seven,
+            R.id.eight,
+            R.id.nine
+        )
+
+        val actionIds = arrayOf(
+            R.id.clear,
+            R.id.delete,
+            R.id.dot,
+            R.id.add,
+            R.id.subtract,
+            R.id.multiply,
+            R.id.divide,
+            R.id.equal
+        )
+
+        val actionButtons = arrayOf(
+            findViewById<Button>(R.id.clear),
+            findViewById(R.id.delete),
+            findViewById(R.id.dot),
+            findViewById(R.id.add),
+            findViewById(R.id.subtract),
+            findViewById(R.id.multiply),
+            findViewById(R.id.divide),
+            findViewById(R.id.equal)
+        )
+
+        for (i in digitIds.indices) {
+            digitButtons[i].setOnClickListener {
+                calculator.digitPressed(digitIds[i])
+                result.text = calculator.outputString
+            }
+        }
+
+        Log.i("digitButtons", "initialazed!")
+
+        for (i in actionIds.indices) {
+            actionButtons[i].setOnClickListener {
+                calculator.actionPressed(actionIds[i])
+                result.text = calculator.outputString
+            }
+        }
+
+        Log.i("actionButtons", "initialazed!")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("saving", "start")
+
+        outState.putString(FIRST_ARG_STR_KEY, calculator.firstArgStr)
+        outState.putDouble(FIRST_ARG_KEY, calculator.firstArgument)
+        outState.putBoolean(FIRST_ARG_HAS_DOT_KEY, calculator.firstArgHasDot)
+
+        outState.putString(SECOND_ARG_STR_KEY, calculator.secondArgStr)
+        outState.putDouble(SECOND_ARG_KEY, calculator.secondArgument)
+        outState.putBoolean(SECOND_ARG_HAS_DOT_KEY, calculator.secondArgHasDot)
+
+        outState.putString(OPERATION_KEY, calculator.operation)
+
+        outState.putString(OUTPUT_STRING_KEY, calculator.outputString)
+
+        Log.i("saving", "successfully")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i("restore", "start")
+        super.onRestoreInstanceState(savedInstanceState)
+
+        calculator.firstArgStr = savedInstanceState.getString(FIRST_ARG_STR_KEY)!!
+        calculator.firstArgument = savedInstanceState.getDouble(FIRST_ARG_KEY)
+        calculator.firstArgHasDot = savedInstanceState.getBoolean(FIRST_ARG_HAS_DOT_KEY)
+
+        calculator.secondArgStr = savedInstanceState.getString(SECOND_ARG_STR_KEY)!!
+        calculator.secondArgument = savedInstanceState.getDouble(SECOND_ARG_KEY)
+        calculator.secondArgHasDot = savedInstanceState.getBoolean(SECOND_ARG_HAS_DOT_KEY)
+
+        calculator.operation = savedInstanceState.getString(OPERATION_KEY)!!
+
+        calculator.outputString = savedInstanceState.getString(OUTPUT_STRING_KEY)!!
+
+        if (calculator.outputString != "") {
+            result.text = calculator.outputString
+        }
+    }
+}
